6a15e35a0095b0957d90048bd4cf213a8e403315
Eric Dorothy <edorothy@topazti.com>
2012-02-17 12:48:48 -0600

    Added NUnit

---
+++ UnitTests/CustomTraceListenerBaseTest.cs
@@ -0,0 +1,16 @@
+﻿using Dacke.TraceListeners;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{   
+    /// <summary>
+    /// This is a test class for CustomTraceListenerBaseTest and is intended
+    /// to contain all CustomTraceListenerBaseTest Unit Tests
+    /// </summary>
+    [TestClass()]
+    public class CustomTraceListenerBaseTest
+    {
+
+    }
+}
