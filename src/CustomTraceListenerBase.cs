ca5ba7607c7f1e57332f6cb73c9ce94fab084134
unknown <Dacke@.(none)>
2012-02-28 23:55:08 -0600

    Modifed the base so that I can start creating the rest of the listeners.

--- src/CustomTraceListenerBase.cs
+++ src/CustomTraceListenerBase.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
 using System.Text;
-using System.Diagnostics;
+
 
 namespace Dacke.TraceListeners
 {
@@ -19,7 +20,7 @@ namespace Dacke.TraceListeners
         /// <param name="message">A message to write. </param>
         public override void Write(string message)
         {
-            base.Write(message);
+            base.Write(PrependPreambleMessage(message));
         }
 
         /// <summary>
@@ -31,10 +32,12 @@ namespace Dacke.TraceListeners
         ///   Write(ex);
         /// </code>
         /// </example>
-        /// <param name="ex">The exeception object that you want to output.</param>
+        /// <param name="ex">The exception object that you want to output.</param>
         public virtual void Write(Exception ex)
         {
-            this.Write(String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message));
+            this.Write(
+               PrependPreambleMessage(
+                   String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message)));
         }
         
         /// <summary>
@@ -43,37 +46,39 @@ namespace Dacke.TraceListeners
         /// <param name="o">An Object whose fully qualified class name you want to write. </param>
         public override void Write(object o)
         {
-            base.Write(o);
+            base.Write(PrependPreambleMessage(o.GetType().FullName));
         }
 
         /// <summary>
-        /// 
+        /// Writes a category name and a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="message"></param>
-        /// <param name="category"></param>
+        /// <param name="message">A message to write.</param>
+        /// <param name="category">A category name used to organize the output</param>
         public override void Write(string message, string category)
         {
-            base.Write(message, category);
+            base.Write(PrependPreambleMessage(message), category);
         }
 
         /// <summary>
-        /// 
+        /// Writes a category name and a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="ex"></param>
-        /// <param name="category"></param>
+        /// <param name="ex">The exception that you want to parse the output of.</param>
+        /// <param name="category">A category name used to organize the output</param>
         public virtual void Write(Exception ex, string category)
         {
-            base.Write(String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message), category);
+            this.Write(
+               PrependPreambleMessage(
+                   String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message)), category);
         }
 
         /// <summary>
-        /// 
+        /// Writes a category name and a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="o"></param>
-        /// <param name="category"></param>
+        /// <param name="o">An object whose full name will be written to the output.</param>
+        /// <param name="category">A category name used to organize the output</param>
         public override void Write(object o, string category)
         {
-            base.Write(o, category);
+            base.Write(PrependPreambleMessage(o.GetType().FullName), category);
         }
 
         #endregion
@@ -81,77 +86,145 @@ namespace Dacke.TraceListeners
         #region WriteLine Overrides
 
         /// <summary>
-        /// 
+        /// Writes a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">A message to write.</param>
         public override void WriteLine(string message)
         {
-            base.WriteLine(GenerateFormattedOutput(message));
+           base.WriteLine(PrependPreambleMessage(message));
         }
 
         /// <summary>
-        /// 
+        /// Writes a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="ex"></param>
+        /// <param name="ex">The exception object whose message and source properties will be output.</param>
         public virtual void WriteLine(Exception ex)
         {
-            this.WriteLine(String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message));
+            this.WriteLine(
+                PrependPreambleMessage(
+                    String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message)));
         }
 
         /// <summary>
-        /// 
+        /// Writes a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="o">An Object name is sent to the Listeners.</param>
         public override void WriteLine(object o)
         {
-            base.WriteLine(o);
+            base.WriteLine(PrependPreambleMessage(o.GetType().FullName));
         }
 
         /// <summary>
-        /// 
+        /// Writes a category name and a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="message"></param>
-        /// <param name="category"></param>
+        /// <param name="message">A message to write</param>
+        /// <param name="category">A category name used to organize the output.</param>
         public override void WriteLine(string message, string category)
         {
-            base.WriteLine(message, category);
+            base.WriteLine(PrependPreambleMessage(message), category);
         }
 
         /// <summary>
-        /// 
+        /// Writes a category name and a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="ex"></param>
-        /// <param name="category"></param>
+        /// <param name="ex">The exception object whose message and source properties will be output.</param>
+        /// <param name="category">A category name used to organize the output.</param>
         public virtual void WriteLine(Exception ex, string category)
         {
-            this.WriteLine(String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message), category);
+            this.WriteLine(
+                PrependPreambleMessage(
+                    String.Format("Source: {0}, Message: {1}", ex.Source, ex.Message)), category);
         }
 
         /// <summary>
-        /// 
+        /// Writes a category name and a message to the trace listeners in the Listeners collection.
         /// </summary>
-        /// <param name="o"></param>
-        /// <param name="category"></param>
+        /// <param name="o">An Object name is sent to the Listeners.</param>
+        /// <param name="category">A category name used to organize the output.</param>
         public override void WriteLine(object o, string category)
         {
-            base.WriteLine(o, category);
+            base.WriteLine(PrependPreambleMessage(o.GetType().FullName), category);
         }
 
         #endregion
 
         #region Protected Methods
 
-        //  TODO: Modify this so that any derived class can override this behavior.
-        protected virtual string GenerateFormattedOutput(object o)
+        /// <summary>
+        /// Gets the information from the stack trace and builds the Preamble message to be 
+        /// attached to any Trace statements that execute through the Write or WriteLine 
+        /// overrides.
+        /// </summary>
+        /// <param name="message">A message to write.</param>
+        /// <returns>The message to write with the pre-pended preamble message.</returns>
+        protected virtual string PrependPreambleMessage(string message)
         {
-            return String.Format("{0} {1} Method: {2}, {3}",
-                                 DateTime.Now.ToShortDateString(),
-                                 DateTime.Now.ToShortTimeString(),
-                                 GetCallingMethodName(),
-                                 o.ToString()
-                );
+            //	Initialize the variable
+            var preambleMessage = String.Empty;
+            
+            try
+            {
+                //	Declare variables
+                StringBuilder preamble = new StringBuilder(Process.GetCurrentProcess().ProcessName + " - ");
+                StackTrace stackTrace = new StackTrace();
+                StackFrame stackFrame;
+                ParameterInfo[] parameters;
+                MethodBase stackFrameMethod;
+                int nframeCount = 0;
+                int parameterIndex = 0;
+                string typeName;
+
+
+                do
+                {
+                    nframeCount++;
+                    stackFrame = stackTrace.GetFrame(nframeCount);
+                    stackFrameMethod = stackFrame.GetMethod();
+                    typeName = stackFrameMethod.ReflectedType.FullName;
+                } while (typeName.StartsWith("System") || typeName.EndsWith("Dacke.Trace"));
+
+                //	Log DateTime, Namespace, Class and Method Name
+                preamble.Append(DateTime.Now.ToUniversalTime().ToString());
+                preamble.Append(": ");
+                preamble.Append(typeName);
+                preamble.Append(".");
+                preamble.Append(stackFrameMethod.Name);
+                preamble.Append("( ");
+
+                // log parameter types and names
+                parameters = stackFrameMethod.GetParameters();
+
+                while (parameterIndex < parameters.Length)
+                {
+                    preamble.Append(parameters[parameterIndex].ParameterType.Name);
+                    preamble.Append(" ");
+                    preamble.Append(parameters[parameterIndex].Name);
+                    parameterIndex++;
+
+                    if (parameterIndex != parameters.Length)
+                    {
+                        preamble.Append(", ");
+                    }
+                }
+
+                preamble.Append(" ): ");
+
+                preambleMessage = preamble.ToString();
+            }
+            catch (Exception ex)
+            {
+                if (Debugger.IsAttached)
+                    Debugger.Break();                
+                throw ex;
+            }
+
+            return (preambleMessage + message);
         }
 
+        /// <summary>
+        /// Returns the name of the calling method.
+        /// </summary>
+        /// <returns>The method name.</returns>
         protected virtual string GetCallingMethodName()
         {
             var callingMethod = MethodBase.GetCurrentMethod().Name;
