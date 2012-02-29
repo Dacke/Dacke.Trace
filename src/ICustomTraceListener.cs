73d37529e04d7c187955560928dae04094d7d02d
unknown <Dacke@.(none)>
2012-02-28 23:59:45 -0600

    Added my first default listener, extracted the methods into an interface and remove the funtionality from the base class

---
+++ src/ICustomTraceListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Dacke.TraceListeners
+{
+    interface ICustomTraceListener
+    {
+        void Write(Exception ex);
+        void Write(Exception ex, string category);
+        void Write(object o);
+        void Write(object o, string category);
+        void Write(string message);
+        void Write(string message, string category);
+        void WriteLine(Exception ex);
+        void WriteLine(Exception ex, string category);
+        void WriteLine(object o);
+        void WriteLine(object o, string category);
+        void WriteLine(string message);
+        void WriteLine(string message, string category);
+    }
+}
